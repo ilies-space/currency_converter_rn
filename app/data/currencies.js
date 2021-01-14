bf3f158d6b370ca296d6627851e8c64ac5cab4a9
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-14 09:35:48 +0100

    Create currencies.js

---
+++ app/data/currencies.js
@@ -0,0 +1,45 @@
+export let currencies = [
+  {
+    name: 'US Dollar',
+    abbreviation: 'USD',
+    symbol: '\u0024',
+    flagURL: 'http://www.geonames.org/flags/l/us.gif',
+  },
+  {
+    name: 'Euro',
+    abbreviation: 'EUR',
+    symbol: '\u20AC',
+    flagURL:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg',
+  },
+  {
+    name: 'Japanese Yen',
+    abbreviation: 'JPY',
+    symbol: '\u00A5',
+    flagURL: 'http://www.geonames.org/flags/l/jp.gif',
+  },
+  {
+    name: 'British Pound',
+    abbreviation: 'GBP',
+    symbol: '\u00A3',
+    flagURL: 'http://www.geonames.org/flags/l/uk.gif',
+  },
+  {
+    name: 'Australian Dollar',
+    abbreviation: 'AUD',
+    symbol: '\u0024',
+    flagURL: 'http://www.geonames.org/flags/l/au.gif',
+  },
+  {
+    name: 'Canadian Dollar',
+    abbreviation: 'CAD',
+    symbol: '\u0024',
+    flagURL: 'http://www.geonames.org/flags/l/ca.gif',
+  },
+  {
+    name: 'Swiss Franc',
+    abbreviation: 'CHF',
+    symbol: '\u0043\u0048\u0046',
+    flagURL: 'http://www.geonames.org/flags/l/ch.gif',
+  },
+];
